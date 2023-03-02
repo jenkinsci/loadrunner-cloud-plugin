bf674c4037668a39e688fa8cc0dfa6b7908979b4
Wei Sun <wei.sun10@microfocus.com>
2023-03-02 23:01:45 +0800

    remove unwanted transient annotation

--- src/main/java/com/microfocus/lrc/core/entity/LoadTestRun.kt
+++ src/main/java/com/microfocus/lrc/core/entity/LoadTestRun.kt
@@ -24,9 +24,7 @@ class LoadTestRun(
     var detailedStatus: String = "NA"
     var status: String = "NA"
     var isTerminated: Boolean = false
-    @Transient
     val reports: MutableMap<String, Int> = mutableMapOf()
-    @Transient
     val reportsByteArray: MutableMap<String, ByteArray> = mutableMapOf()
     var startTime: Long = -1
     var endTime: Long = -1
